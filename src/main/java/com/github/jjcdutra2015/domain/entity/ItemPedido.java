2a78815dd42a8ad5dad98cceae29c88d767c4efa
Julio Dutra <jjcdutra@gmail.com>
2021-07-24 11:15:30 -0300

    Criando as classes de modelo

---
+++ src/main/java/com/github/jjcdutra2015/domain/entity/ItemPedido.java
@@ -0,0 +1,41 @@
+package com.github.jjcdutra2015.domain.entity;
+
+public class ItemPedido {
+
+    private Integer id;
+    private Cliente cliente;
+    private Produto produto;
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}

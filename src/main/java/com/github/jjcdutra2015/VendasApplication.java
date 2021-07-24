57174c67f039ae9cdf6c3c8508eb1a35abcbe27c
Julio Dutra <jjcdutra@gmail.com>
2021-07-24 11:48:01 -0300

    Salvando e recuperando cliente

--- src/main/java/com/github/jjcdutra2015/VendasApplication.java
+++ src/main/java/com/github/jjcdutra2015/VendasApplication.java
@@ -1,10 +1,25 @@
 package com.github.jjcdutra2015;
 
+import com.github.jjcdutra2015.domain.entity.Cliente;
+import com.github.jjcdutra2015.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            clientes.salvar(new Cliente("Julio"));
+            clientes.salvar(new Cliente("Jose"));
+
+            clientes.obterTodos().forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);

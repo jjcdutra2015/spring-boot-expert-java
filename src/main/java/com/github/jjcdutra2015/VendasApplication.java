7ea9714f732774255e6ca6462d2fbe7b1fa34ec8
Julio Dutra <jjcdutra@gmail.com>
2021-07-26 20:55:07 -0300

    Concluindo o cadastro de clientes

--- src/main/java/com/github/jjcdutra2015/VendasApplication.java
+++ src/main/java/com/github/jjcdutra2015/VendasApplication.java
@@ -14,10 +14,31 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
-            clientes.salvar(new Cliente("Julio"));
-            clientes.salvar(new Cliente("Jose"));
 
+            System.out.println("Salvando cliente");
+            clientes.salvar(new Cliente("Julio"));
+            clientes.salvar(new Cliente("Julia"));
             clientes.obterTodos().forEach(System.out::println);
+
+            System.out.println("Atualizando cliente");
+            clientes.obterTodos().forEach(c -> {
+                c.setNome(c.getNome() + " atualizado");
+                clientes.atualizar(c);
+            });
+            clientes.obterTodos().forEach(System.out::println);
+
+            System.out.println("Buscando cliente por nome");
+            clientes.buscarPorNome("Ju").forEach(System.out::println);
+
+//            System.out.println("Deletando cliente");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+            if (clientes.obterTodos().isEmpty()) {
+                System.out.println("Nenhum cliente encontrado");
+            } else {
+                clientes.obterTodos().forEach(System.out::println);
+            }
         };
     }
 

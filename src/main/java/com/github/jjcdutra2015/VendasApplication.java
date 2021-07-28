5194b24b5c045d5b2dffbf2507f392096d544651
Julio Dutra <jjcdutra@gmail.com>
2021-07-28 20:50:41 -0300

    Criando o controller de clientes

--- src/main/java/com/github/jjcdutra2015/VendasApplication.java
+++ src/main/java/com/github/jjcdutra2015/VendasApplication.java
@@ -1,43 +1,10 @@
 package com.github.jjcdutra2015;
 
-import com.github.jjcdutra2015.domain.entity.Cliente;
-import com.github.jjcdutra2015.domain.entity.Pedido;
-import com.github.jjcdutra2015.domain.repository.Clientes;
-import com.github.jjcdutra2015.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes, @Autowired Pedidos pedidos) {
-        return args -> {
-
-            System.out.println("Salvando cliente");
-            Cliente julio = new Cliente("Julio");
-            clientes.save(julio);
-
-            Pedido p = new Pedido();
-            p.setCliente(julio);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente = clientes.findClienteFetchPedidos(julio.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-            pedidos.findByCliente(julio).forEach(System.out::println);
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);

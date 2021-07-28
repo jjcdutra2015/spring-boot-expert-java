73dfe156d4530c0efbf8fb6f260829694d2e004c
Julio Dutra <jjcdutra@gmail.com>
2021-07-28 20:30:14 -0300

    Carregando os pedidos por cliente

--- src/main/java/com/github/jjcdutra2015/VendasApplication.java
+++ src/main/java/com/github/jjcdutra2015/VendasApplication.java
@@ -31,9 +31,11 @@ public class VendasApplication {
 
             pedidos.save(p);
 
-            Cliente cliente = clientes.findClienteFetchPedidos(julio.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos());
+//            Cliente cliente = clientes.findClienteFetchPedidos(julio.getId());
+//            System.out.println(cliente);
+//            System.out.println(cliente.getPedidos());
+
+            pedidos.findByCliente(julio).forEach(System.out::println);
         };
     }
 
--- src/main/java/com/github/jjcdutra2015/domain/repository/Pedidos.java
+++ src/main/java/com/github/jjcdutra2015/domain/repository/Pedidos.java
@@ -1,7 +1,12 @@
 package com.github.jjcdutra2015.domain.repository;
 
+import com.github.jjcdutra2015.domain.entity.Cliente;
 import com.github.jjcdutra2015.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
+
+    List<Pedido> findByCliente(Cliente cliente);
 }

ee2689c56c395b760c2536b65f5d13a829195236
Julio Dutra <jjcdutra@gmail.com>
2021-07-29 20:06:05 -0300

    Requisição GET com parametros e ResponseEntity

--- src/main/java/com/github/jjcdutra2015/VendasApplication.java
+++ src/main/java/com/github/jjcdutra2015/VendasApplication.java
@@ -1,10 +1,23 @@
 package com.github.jjcdutra2015;
 
+import com.github.jjcdutra2015.domain.entity.Cliente;
+import com.github.jjcdutra2015.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            Cliente c = new Cliente("Julio");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);

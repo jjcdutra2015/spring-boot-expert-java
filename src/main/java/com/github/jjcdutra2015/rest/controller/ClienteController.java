1aa7e0aa6e10c8b5c115f960dbb25ad34b4b7c8e
Julio Dutra <jjcdutra@gmail.com>
2021-07-29 21:29:23 -0300

    Buscando cliente por parametro

--- src/main/java/com/github/jjcdutra2015/rest/controller/ClienteController.java
+++ src/main/java/com/github/jjcdutra2015/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package com.github.jjcdutra2015.rest.controller;
 
 import com.github.jjcdutra2015.domain.entity.Cliente;
 import com.github.jjcdutra2015.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -53,5 +56,16 @@ public class ClienteController {
             clientes.save(cliente);
             return ResponseEntity.noContent().build();
         }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity<?> find(Cliente filtro) {
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Cliente> example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }

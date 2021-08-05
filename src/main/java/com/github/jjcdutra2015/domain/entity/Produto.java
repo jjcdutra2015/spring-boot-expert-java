ffbefb95e11cf36a40428eb4d4a097b692f81dfc
Julio Dutra <jjcdutra@gmail.com>
2021-08-05 20:14:56 -0300

    Validando a entidade produto

--- src/main/java/com/github/jjcdutra2015/domain/entity/Produto.java
+++ src/main/java/com/github/jjcdutra2015/domain/entity/Produto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
@@ -20,8 +22,10 @@ public class Produto {
     private Integer id;
 
     @Column(name = "descricao")
+    @NotEmpty(message = "Campo descrição é obrigatório")
     private String descricao;
 
     @Column(name = "preco_unitario")
+    @NotNull(message = "Campo preço é obrigatório")
     private BigDecimal preco;
 }

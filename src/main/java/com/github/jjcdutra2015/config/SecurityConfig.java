b71878f0166e787269e62c83b256e7e424c0603f
Julio Dutra <jjcdutra@gmail.com>
2021-08-07 14:05:32 -0300

    Testando autenticação com cadastro de usuários

--- src/main/java/com/github/jjcdutra2015/config/SecurityConfig.java
+++ src/main/java/com/github/jjcdutra2015/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.github.jjcdutra2015.config;
 import com.github.jjcdutra2015.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,6 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .hasAnyRole("ADMIN", "USER")
                 .antMatchers("/api/produtos/**")
                 .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                .permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }

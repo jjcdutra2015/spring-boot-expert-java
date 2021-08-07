d116ad0f2d2b26dbcdc04cc80d6359dbc0b485c5
Julio Dutra <jjcdutra@gmail.com>
2021-08-07 09:47:50 -0300

    Autenticação Basic

--- src/main/java/com/github/jjcdutra2015/config/SecurityConfig.java
+++ src/main/java/com/github/jjcdutra2015/config/SecurityConfig.java
@@ -36,6 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/produtos/**")
                 .hasRole("ADMIN")
                 .and()
-                .formLogin();
+                .httpBasic();
     }
 }
